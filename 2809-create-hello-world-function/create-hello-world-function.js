653dc81dec75ef470a1f98a9027a89a5eb600809
Sayantan Choudhury <123821730+Arko298@users.noreply.github.com>
2024-08-21 09:33:46 +0530

    Time: 46 ms (79.95%) | Memory: 48.3 MB (86.67%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,17 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    const greetings= "Hello World"
+    
+    return function() {
+        return greetings
+        
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
